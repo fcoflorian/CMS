adeb8dd992e99a7015db2036c39262520c0a0410
Francisco Antonio Florian Mercado <fco.florian@gmail.com>
2017-12-04 00:03:38 -0400

    ahora si

--- application/views/vista/galeria_view.php
+++ application/views/vista/galeria_view.php
@@ -21,7 +21,7 @@
             <?php foreach($fotos as $foto){ ?>
             <div class="col col-md-4">
               <div class="card mb-3 mr-5">
-                <img class="card-img-top img-fluid" src="<?php echo base_url('/imagenes/');?><?php echo isset($foto['foto'])?$foto['foto']:'';?>" alt="Card image cap">
+                <img class="card-img-top" width="250" height="250" src="<?php echo base_url('/imagenes/');?><?php echo isset($foto['foto'])?$foto['foto']:'';?>" alt="Card image cap">
                 <div class="card-body">
                   <h4 class="card-title"><?php echo $foto['nombre'] ?></h4>
                   <p class="card-text"><?php echo $foto['descripcion'] ?></p>
